f7817155587614efaca8144a32793a31c57a8b1e
andejuli <techtrek@hotmail.com>
2025-02-18 09:28:35 -0700

    form

---
+++ form/form-demo.js
@@ -0,0 +1,19 @@
+function togglePaymentDetails(e) {
+    // get a reference to the form. We can access all the named form inputs through the form element.
+    const theForm = ;
+    // we will also need the creditCardContainer and paypalUsernameContainer
+    const creditCardContainer = ;
+    const paypalContainer = ;
+  
+    // Hide payment containers by adding the '.hide' class to each of them
+  
+    // Disable required for payment fields...if we hide a required field the browser will throw an error when we try to submit!
+  
+  
+    // Show the container based on the selected payment method, and add the required attribute back.
+  
+  }
+
+// attach a change event handler to the paymentMethod input
+
+// attach a submit event handler to the form
