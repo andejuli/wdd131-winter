313c8e91d9716e5b4ee868a08cec1d2bd7112d7e
andejuli <techtrek@hotmail.com>
2025-04-30 13:07:45 -0600

    wk2

---
+++ ponder/jspractice.js
@@ -0,0 +1,40 @@
+// variables vs constants
+const PI = 3.14;
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+// type coersion
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+
+// scope
+let global = "I'm global";
+
+function exampleFunction() {
+    let block = "I am block level or local";
+    console.log(block);
+    console.log(global);
+}
+
+//console.log(block);
+console.log(global);
+exampleFunction();
+
+//globle = "I am also global";
+
+console.log(globle);
---
+++ ponder/target.js
@@ -0,0 +1,5 @@
+const para1 = document.querySelector('#intro');
+
+para1.style.backgroundColor = "#f2c556";
+
+let img_area = document.querySelector('#starship');
