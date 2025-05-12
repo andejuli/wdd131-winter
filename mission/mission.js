4802b03e84a727d408bb7707a0daa0d967572add
andejuli <techtrek@hotmail.com>
2025-05-12 10:25:31 -0600

    debug

--- mission/mission.js
+++ mission/mission.js
@@ -1,16 +1,21 @@
 let selectElem = document.querySelector('select');
+
 let logo = document.querySelector('img');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
+
     if (current == 'dark'){
+        // give body the dark class
         document.body.className = 'dark';
-        document.body.className = 'dark';
+        // add different image by changing src
         logo.setAttribute('src', 'byui-logo_white.png');
     } else {
+        // take off class
         document.body.classList.remove('dark');
-        logo.setAttribute('src', 'byui-logo.webp');
+        // change the image back to original
+        logo.setAttribute('src', 'byui-logo.webp')
     }
 }
