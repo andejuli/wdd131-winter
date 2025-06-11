2fe296b55ce0825b61cdd0fdacbe3dc08a0ddfa6
andejuli <techtrek@hotmail.com>
2025-06-11 15:55:17 -0600

    char-card

--- ponder/form-demo.js
+++ ponder/form-demo.js
@@ -3,13 +3,13 @@ let paypalContainer = document.querySelector('#paypalUsernameContainer')
 
 function togglePaymentDetails() {
      
-        // let creditInput = document.querySelector('#creditCardNumber');
-        // let paypalInput = document.querySelector('#paypalUsername');
+        let creditInput = document.querySelector('#creditCardNumber');
+        let paypalInput = document.querySelector('#paypalUsername');
 
         let value = document.querySelector("#paymentMethod").value;
 
-        // paypalInput.required=false; //theForm.paypalUsername.required = false;
-        // creditInput.required=false; 
+        paypalInput.required=false; //theForm.paypalUsername.required = false;
+ 
 
         paypalContainer.classList.add('hide');
         creditCardContainer.classList.add('hide');
@@ -17,16 +17,14 @@ function togglePaymentDetails() {
         if (value == 'creditCard') {
           creditCardContainer.classList.remove('hide');
 
-          // creditInput.required=true;  
+          creditInput.required=true;  
 
         } else if (value == 'paypal') {
           paypalContainer.classList.remove('hide');
           
-          // paypalInput.required=true;  
+          paypalInput.required=true;  
         }
       }
-// attach a change event handler to the paymentMethod input
-const selectElement = document.getElementById("paymentMethod");
 
-// attach a submit event handler to the form
+const selectElement = document.getElementById("paymentMethod");
 selectElement.addEventListener('change', togglePaymentDetails);
